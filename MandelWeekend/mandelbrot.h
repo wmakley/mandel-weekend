28777c11e56bc14b4d04ae19fc594b6d725c5e94
William Makley <wmakley@tortus.com>
2013-11-29 10:02:56 -0500

    Add mandel_brot_escape_time C library

---
+++ MandelWeekend/mandelbrot.h
@@ -0,0 +1,14 @@
+//
+//  mandelbrot.h
+//  MandelWeekend
+//
+//  Created by William Makley on 11/28/13.
+//  Copyright (c) 2013 William Makley. All rights reserved.
+//
+
+#ifndef MandelWeekend_mandelbrot_h
+#define MandelWeekend_mandelbrot_h
+
+int mandelbrot_escape_time(float x, float y, int max_iterations);
+
+#endif
