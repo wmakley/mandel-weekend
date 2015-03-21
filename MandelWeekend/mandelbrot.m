485f1ef8a6c30c9e49bb90244c5dabf2e57c7690
William Makley <wmakley@tortus.com>
2015-03-20 23:48:03 -0400

    add more comments to mandelbrot_point_for_pixel

--- MandelWeekend/mandelbrot.m
+++ MandelWeekend/mandelbrot.m
@@ -29,9 +29,23 @@ NSRect zoom_in_on_rect(NSRect base, NSRect zoom) {
                       base.size.height * zoom.size.height);
 }
 
+// Convert point "pixel" in screen of dimensions "screen" to coordinates
+// in screen of dimensions mandelbrot_space.
+//
+// Imagine mandelbrot_space as being a graph super-imposed on the screen,
+// where screen width is equivalent to mandelbrot width, and the mandebrot
+// origin in the center of the screen.
+//
+// Another way to think of it: mandelbrot_space is just the size of the graph
+// to draw the fractical on, and its origin is how much to translate the
+// fractal.
+//
+// Really we are peforming a translation and a scaling on the pixel defined a rect,
+// and my own intuitive train of thoughts on what to multiply by what, when
+// this should really just be matrices or NSAffineTransformations or something.
 NSPoint mandelbrot_point_for_pixel(NSPoint pixel, NSSize screen, NSRect mandelbrot_space) {    
     CGFloat x_offset = mandelbrot_space.origin.x - (mandelbrot_space.size.width / 2);
-    CGFloat x = pixel.x / screen.width * mandelbrot_space.size.width + x_offset;
+    CGFloat x = ((pixel.x / screen.width) * mandelbrot_space.size.width) + x_offset;
     
     CGFloat y_offset = mandelbrot_space.origin.y - (mandelbrot_space.size.height / 2);
     CGFloat y = pixel.y / screen.height * mandelbrot_space.size.height + y_offset;
